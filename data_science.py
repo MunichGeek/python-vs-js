bde9547d614df2c2faae41ac353502018f695e30
MunichGeek <munichfarid@gmail.com>
2022-11-27 12:43:48 +0100

    Minor imporovements.

--- data_science.py
+++ data_science.py
@@ -6,8 +6,8 @@ x = [6, 9, 7, 7, 4, 19, 0, 8, 3, 11, 13, 10, 6]
 y = [98, 87, 86, 88, 90, 88, 94, 88, 93, 80, 82, 87, 86]
 
 slope, intercept, _, _, _ = stats.linregress(x, y)
-linear_ml = list(map(lambda x_i: slope * x_i + intercept, x))
+linear_model = list(map(lambda x_i: slope * x_i + intercept, x))
 
 plt.scatter(x, y)
-plt.plot(x, linear_ml)
+plt.plot(x, linear_model)
 # %%
--- hash_table.py
+++ hash_table.py
@@ -3,9 +3,9 @@ import timeit, random
 def find_entry_in_structure(structure_to_search, value_to_find):
     return (value_to_find in structure_to_search)
 
-data_structure = list()
-for i in range(1000):
-    data_structure.append(random.random())
+data_structure = set()
+for _ in range(1000):
+    data_structure.add(random.random())
 # print(data_structure)
 
 time_to_run = timeit.timeit("find_entry_in_structure(data_structure, 1.0)", number=int(1e6), globals=globals())
