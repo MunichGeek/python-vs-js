f7e4f4d6ba3b91e5307ba0c81e683efb4b1cf1aa
MunichGeek <munichfarid@gmail.com>
2022-11-27 11:34:30 +0100

    Adding ML-script data-science.

--- data_science.py
+++ data_science.py
@@ -1,13 +1,13 @@
-# %% 
+# %%
 import matplotlib.pyplot as plt
 from scipy import stats
 
 x = [6, 9, 7, 7, 4, 19, 0, 8, 3, 11, 13, 10, 6]
-y = [101, 87, 86, 88, 113, 88, 102, 88, 93, 80, 77, 87, 86]
+y = [98, 87, 86, 88, 90, 88, 94, 88, 93, 80, 82, 87, 86]
 
 slope, intercept, _, _, _ = stats.linregress(x, y)
-mymodel = list(map(lambda x:slope * x + intercept, x))
+linear_ml = list(map(lambda x_i: slope * x_i + intercept, x))
 
 plt.scatter(x, y)
-plt.plot(x, mymodel)
+plt.plot(x, linear_ml)
 # %%
